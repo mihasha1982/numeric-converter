20c673fe02e25eb722e61530b8c598213f2e8db2
Mihael <80133536+mihasha1982@users.noreply.github.com>
2021-03-08 20:28:33 +0300

    Update code2.h

--- code2.h
+++ code2.h
@@ -1,6 +1,43 @@
+//программа переводит число из одной системы счисления в другую, 
+//систему счисления исходного и конечного числа пользователь выбирает сам
 #include <stdio.h>
-//функция
-int save_up(char number[],int base)
+//прототипы функций обязательно должны быть до главной функции main
+int convert_array_called_number_to_decimal_number(char[],int);
+void convert_variable_called_number10_and_put_array_number_end(char[],int,int);
+void swap_values_in_number_end_so_that_last_one_becomes_the_first_one_and_so_on(char[]);
+
+int main()
+{
+  char number[50] = {0},
+       number_end[50] = {0},
+       change = 0;
+   int base = 0,
+       base2 = 0,
+       number10 = 0;
+
+
+  printf("Enter number:");
+  fgets(number,50,stdin);
+
+  printf("Enter base:");
+  scanf("%d",&base);
+
+  //применяю функцию
+  number10 = convert_array_called_number_to_decimal_number(number,base);
+
+  printf("Enter output base:");
+  scanf("%d",&base2);
+
+  //применяю функцию 2
+  convert_variable_called_number10_and_put_array_number_end(number_end,number10,base2);
+
+  //применяю функцию 3
+  swap_values_in_number_end_so_that_last_one_becomes_the_first_one_and_so_on(number_end);
+
+  printf("%s\n",number_end);
+  return 0;
+}
+int convert_array_called_number_to_decimal_number(char number[],int base)
 {
   int num10 = 0;
 
@@ -14,62 +51,28 @@ int save_up(char number[],int base)
   return num10;
 }
 //функция2
-void fill_array(char number[],int num,int base)
+void convert_variable_called_number10_and_put_array_number_end(char number[],int num,int base)
 {
   for(int i = 0; i<50 && num!=0 ;i++)
-       {
+     {
          number[i] = (num%base<10)
          ?num%base+'0'
          :num%base+'A'-10;
          num = num/base;
-       }
+     }
 
 }
 //функция 3
-void swap_places(char number[],int i, int g)
+void swap_values_in_number_end_so_that_last_one_becomes_the_first_one_and_so_on(char number[])
 {
-  char change = 0;
+  char temp = 0;
+  int i = -1,
+      g = 0;
+  while(number[i]!='\0')i++;
   for(i;i>g;i--,g++)
-  {
-    change = number[i];
-    number[i] = number[g];
-    number[g] = change;
-  }
+    {
+         temp = number[i];
+         number[i] = number[g];
+         number[g] = temp;
+    }
 }
-
-int main()
-{
-  char number[50] = {0},
-       number_end[50] = {0},
-       change = 0;
-   int base = 0,
-       base2 = 0,
-       number10 = 0,
-       i,k,
-       g = 0;
-
-
-  printf("Enter number:");
-  fgets(number,50,stdin);
-
-  printf("Enter base:");
-  scanf("%d",&base);
-
-  //применяю функцию
-  number10 = save_up(number,base);
-
-  printf("Enter output base:");
-  scanf("%d",&base2);
-
-  //применяю функцию 2
-  fill_array(number_end,number10,base2);
-
-  i--;
-  //применяю функцию 3
-  swap_places(number_end,i,g);
-
-  printf("%s\n",number_end);
-  return 0;
-}
-
-
