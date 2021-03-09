56cd053c942bfe9bbe8e1a011b244b498c566236
Mihael <80133536+mihasha1982@users.noreply.github.com>
2021-03-09 23:32:55 +0300

    Update code2.h

--- code2.h
+++ code2.h
@@ -1,16 +1,18 @@
 //Программа переводит число из одной системы счисления в другую. 
 //Системы счисления пользователь выбирает сам
 #include <stdio.h>
-int convert_array_to_decimal_number(char[],int);
-void convert_variable_to_array(char[],int,int);
+
+int convert_array_to_decimal_number(char[], int);
+void convert_variable_to_array(char[], int, int);
 void swap_values_in_array(char[]);
 
-int main(){
-  char number[50] = {0},
-       number_end[50] = {0};
-   int base = 0,
-       base2 = 0,
-       number10 = 0;
+int main() {
+    char number[50] = {0},
+         number_end[50] = {0};
+  
+    int base = 0,
+        base2 = 0,
+        number10 = 0;
 
     printf("Enter number:");
     fgets(number, 50, stdin);
@@ -30,33 +32,42 @@ int main(){
     printf("%s\n", number_end);
     return 0;
 }
-int convert_array_to_decimal_number(char number[], int base){
-  int num = 0;
-    for(int i = 0; i < 50 && number[i] != '\n';i++){
-          num = number[i] < 'A'
-          ? num * base + number[i] - '0'
-          : num * base + number[i] - 'A' + 10;
-          }
+
+int convert_array_to_decimal_number(char number[], int base) {
+    
+    int num = 0;
+  
+    for (int i = 0; i < 50 && number[i] != '\n';i++) {
+              num = number[i] < 'A'
+              ? num * base + number[i] - '0'
+              : num * base + number[i] - 'A' + 10;
+    }
+  
           return num;
-          }
-//функция2
-void convert_variable_to_array(char number[], int num, int base){
-    for(int i = 0; i < 50 && num != 0 ;i++){
-         number[i] = num % base < 10
-         ? num % base + '0'
-         : num % base + 'A' - 10;
-         num = num / base;
-         }
-         }
-//функция 3
-void swap_values_in_array(char number[]){
-     char temp = 0;
-      int i = -1,
-          g = 0;
-     while(number[i] != '\0')i++;
-     for(i; i > g; i--, g++){
-         temp = number[i];
-         number[i] = number[g];
-         number[g] = temp;
-         }
-         }
+}
+
+void convert_variable_to_array(char number[], int num, int base) {
+    
+    for (int i = 0; i < 50 && num != 0 ;i++) {
+             number[i] = num % base < 10
+             ? num % base + '0'
+             : num % base + 'A' - 10;
+             num = num / base;
+    }
+}
+
+void swap_values_in_array(char number[]) {
+    
+    char temp = 0;
+    
+    int i = -1,
+        g = 0;
+  
+    while (number[i] != '\0')i++;
+  
+    for (i; i > g; i--, g++) {
+             temp = number[i];
+             number[i] = number[g];
+             number[g] = temp;
+    }
+}
